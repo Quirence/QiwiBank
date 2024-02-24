8487f15e0d7fbd253d68c6f455b91bfce9b42d75
Quirence <shades.h4ndeir@gmail.com>
2024-02-24 21:41:47 +0800

    Updated

--- database/users.py
+++ database/users.py
@@ -1,9 +1,8 @@
 import sqlite3
 import os
-
+import hashlib
 current_dir = os.path.dirname(os.path.abspath(__file__))
 database_path = os.path.join(current_dir, 'users.db')
-
 
 def add_user(username, email, password):
     conn = sqlite3.connect(database_path)
@@ -13,13 +12,14 @@ def add_user(username, email, password):
     cursor.execute(
         '''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, email TEXT, password TEXT)''')
 
+    # Хешируем пароль
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
     # Добавляем пользователя в базу данных
-    cursor.execute('''INSERT INTO users (username, email, password) VALUES (?, ?, ?)''', (username, email, password))
+    cursor.execute('''INSERT INTO users (username, email, password) VALUES (?, ?, ?)''', (username, email, hashed_password))
 
     conn.commit()
     conn.close()
-
-
 def get_users():
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
@@ -30,3 +30,14 @@ def get_users():
     conn.close()
 
     return users
+
+def get_user_by_username(username):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+
+    cursor.execute('''SELECT * FROM users WHERE username = ?''', (username,))
+    user = cursor.fetchone()
+
+    conn.close()
+
+    return user
--- web_app/app.py
+++ web_app/app.py
@@ -1,103 +1,88 @@
-# Импорт необходимых модулей
+# app.py
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
-from routes import handle_registration_form, handle_login_form, handle_main_page
+from routes import handle_registration_form, handle_login_form, handle_main_page, handle_register, handle_login
 from database.users import add_user, get_users
+import hashlib
 
-# Класс для обработки запросов
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    # Структура для хранения сессий
     sessions = {}
 
     def do_GET(self):
         if self.path == '/login':
-            session_id = self.headers.get('Cookie')
-            if session_id and session_id in self.sessions and self.sessions[session_id]:
-                self.send_response(302)
-                self.send_header('Location', '/main_page')  # Перенаправляем на главную страницу
-                self.end_headers()
-            else:
-                handle_login_form(self)
+            self.handle_login_form()
         elif self.path == '/main_page':
             session_id = self.headers.get('Cookie')
             if session_id and session_id in self.sessions and self.sessions[session_id]:
-                handle_main_page(self)
+                self.handle_main_page()
             else:
                 self.send_response(302)
-                self.send_header('Location', '/login')  # Перенаправляем на страницу входа
+                self.send_header('Location', '/login')
                 self.end_headers()
         else:
-            handle_registration_form(self)
+            self.handle_registration_form()
 
     def do_POST(self):
         if self.path == '/register':
+            self.handle_register()
+        elif self.path == '/login':
             content_length = int(self.headers['Content-Length'])
             post_data = self.rfile.read(content_length).decode('utf-8')
             data_dict = parse_qs(post_data)
-            username = data_dict.get('username', [''])[0]
-            email = data_dict.get('email', [''])[0]
-            password = data_dict.get('password', [''])[0]
-            confirmPassword = data_dict.get('confirmPassword', [''])[0]
+            self.handle_login(data_dict)
 
-            # Проверка на пустые поля
-            if not (username and email and password and confirmPassword):
-                self.send_response(400)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b'Please fill out all fields!')
-                return
+    def handle_registration_form(self):
+        handle_registration_form(self)
 
-            if password != confirmPassword:
-                self.send_response(400)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b'Passwords do not match!')
-                return
+    def handle_login_form(self):
+        handle_login_form(self)
 
-            add_user(username, email, password)  # Добавление пользователя в базу данных
-
-            # Перенаправляем пользователя на страницу авторизации после успешной регистрации
+    def handle_main_page(self):
+        session_id = self.headers.get('Cookie')
+        if session_id and session_id in self.sessions and self.sessions[session_id]:
+            handle_main_page(self)
+        else:
             self.send_response(302)
             self.send_header('Location', '/login')
             self.end_headers()
-        elif self.path == '/login':
-            print("Received login POST request")
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length).decode('utf-8')
-            data_dict = parse_qs(post_data)
-            username = data_dict.get('username', [''])[0]
-            password = data_dict.get('password', [''])[0]
 
-            # Получаем список пользователей из базы данных
-            user_list = get_users()
+    def handle_register(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        data_dict = parse_qs(post_data)
+        handle_register(self, data_dict)
 
-            # Проверяем, существует ли пользователь с таким логином и паролем
-            if (username, password) in [(user[1], user[3]) for user in user_list]:
-                print("User authenticated successfully")
-                session_id = self.headers.get('Cookie')
-                if not session_id:
+    def handle_login(request_handler, data_dict):
+        username = data_dict.get('username', [''])[0]
+        password = data_dict.get('password', [''])[0]
+
+        user_list = get_users()
+
+        # Хешируем введенный пароль
+        hashed_password_input = hashlib.sha256(password.encode()).hexdigest()
+
+        # Проверяем, есть ли пользователь с таким логином
+        for user in user_list:
+            if user[1] == username:
+                hashed_password_db = user[3]
+                # Сравниваем хеши паролей
+                if hashed_password_db == hashed_password_input:
+                    # Устанавливаем сессию для пользователя
                     session_id = str(hash(username + password))
-                    self.sessions[session_id] = True  # Устанавливаем сессию
-                    print("Session created:", session_id)
-                    self.send_response(302)
-                    self.send_header('Set-Cookie', session_id)
-                    self.send_header('Location', '/main_page')
-                    self.end_headers()
-            else:
-                print("Authentication failed")
-                self.send_response(401)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b'Unauthorized Access!')
+                    request_handler.sessions[session_id] = True
+                    request_handler.send_response(302)
+                    request_handler.send_header('Set-Cookie', session_id)
+                    request_handler.send_header('Location', '/main_page')
+                    request_handler.end_headers()
+                    return
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Starting server on port {port}...')
-    httpd.serve_forever()
+        # Если не найден пользователь или пароли не совпадают
+        request_handler.send_response(401)
+        request_handler.send_header('Content-type', 'text/html')
+        request_handler.end_headers()
+        request_handler.wfile.write(b'Unauthorized Access!')
 
-if __name__ == '__main__':
-    run()
+
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
