ee354aeb0a79a49e67ae2dccdc3025a632643a5f
CrazyDogCTRL <CrazyDogCTRL@yandex.ru>
2024-03-27 22:51:32 +0800

    Добавлено куки

--- web_app/app.py
+++ web_app/app.py
@@ -1,10 +1,16 @@
 import socket
+from http import cookies
 from views import *
 from urllib.parse import unquote_plus
 from Control.control import *
+import time
 
+# Глобальные переменные
 HOST = ("localhost", 7777)
 control = Control()
+active_sessions = {}
+COOKIE_NAME = "session_id"
+
 URLS = {
     "/": home,
     "/login": login,
@@ -18,7 +24,6 @@ URLS = {
     "/deposit": deposit
 }
 
-
 POST_urls = {
     "/login": login,
     "/register": register,
@@ -26,63 +31,79 @@ POST_urls = {
 }
 
 
-def generate_headers(method, url):
-    if method == "POST" and url not in POST_urls:
-        return "HTTP/1.1 405 Method not allowed\nContent-Type: text/html; charset=utf-8\n\n", 405
+def check_session_validity(session_id):
+    if session_id in active_sessions:
+        session_start_time = active_sessions[session_id]["start_time"]
+        if time.time() - session_start_time <= 3 * 3600:
+            return True
+    return False
+
+
+def generate_session_id():
+    return str(time.time())
+
+
+def set_cookie_header(session_id):
+    cookie = cookies.SimpleCookie()
+    cookie[COOKIE_NAME] = session_id
+    cookie[COOKIE_NAME]["path"] = "/"
+    cookie[COOKIE_NAME]["max-age"] = 3 * 3600  # expire after 3 hours
+    return cookie.output(header="").strip()
+
+
+def redirect_to(location, session_id):
+    return f"HTTP/1.1 302 Found\nLocation: {location}\nSet-Cookie: {set_cookie_header(session_id)}\nContent-Type: text/html; charset=utf-8\n\n"
+
+
+def redirect_main(session_id):
+    # Перенаправление на главную страницу, если пользователь не вошел в систему
+    new_location = "/main"
+    return redirect_to(new_location, session_id)
+
+
+def generate_headers(method, url, session_id):
     if url not in URLS:
         return "HTTP/1.1 404 Not found\nContent-Type: text/html; charset=utf-8\n\n", 404
 
     if url == "/login" and method == "POST":
-        new_location = "http://localhost:7777/main"
-        return f"HTTP/1.1 302 Found\nLocation: {new_location}\nContent-Type: text/html; charset=utf-8\n\n", 302
+        new_location = "http://localhost:7777/command"  # Перенаправляем на /command после успешной аутентификации
+        return redirect_to(new_location, session_id), 302
+
+    if url != "/login" and url != "/register" and url != "/main" and not check_session_validity(session_id):
+        return redirect_main(session_id), 302
+
+    if method == "POST" and url not in POST_urls:
+        return "HTTP/1.1 405 Method not allowed\nContent-Type: text/html; charset=utf-8\n\n", 405
 
     return "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n", 200
 
 
 def parse_request(request):
-    # Разбиваем запрос на строки
     lines = request.split("\r\n")
-
-    # Первая строка содержит метод, URL и версию HTTP (если указана)
     first_line_parts = lines[0].split(" ")
     method = first_line_parts[0]
     url = first_line_parts[1]
     version = first_line_parts[2] if len(first_line_parts) > 2 else None
-
-    # Ищем индекс пустой строки, разделяющей заголовки и тело запроса
     index = None
     for i, line in enumerate(lines):
         if line == "":
             index = i
             break
-
-    # Если пустая строка не найдена, то это GET запрос без тела
     if index is None:
         headers = {}
         data = {}
     else:
-        # Заголовки запроса
         headers = {}
         for line in lines[1:index]:
-            key, value = line.split(": ", 1)  # Разделяем строку только по первому символу ":"
+            key, value = line.split(": ", 1)
             headers[key] = value
-
-        # Тело POST-запроса
         body = "\r\n".join(lines[index + 1:])
-
-        # Инициализируем словарь для хранения данных
         data = {}
-
-        # Если это POST-запрос, парсим тело запроса
         if method == "POST":
-            # Разбиваем тело запроса на параметры
             params = body.split("&")
-
-            # Добавляем параметры в словарь
             for param in params:
                 key, value = param.split("=")
-                data[unquote_plus(key)] = unquote_plus(value)  # декодируем ключи и значения
-
+                data[unquote_plus(key)] = unquote_plus(value)
     return method, url, headers, data
 
 
@@ -96,22 +117,23 @@ def generate_content(code, url):
 
 def generate_result(request):
     method, url, headers, data = parse_request(request)
-    if method != "GET":
-        print(data)
-    headers, code = generate_headers(method, url)
-    # Если регистрация или аутентификация прошли успешно, выполняем редирект
+    session_id = None
+    if "Cookie" in headers:
+        cookie = cookies.SimpleCookie(headers["Cookie"])
+        if COOKIE_NAME in cookie:
+            session_id = cookie[COOKIE_NAME].value
+
+    headers, code = generate_headers(method, url, session_id)
     if (url == '/register' or url == '/login') and method == "POST":
         control_response = control.treatment_request(data)
-        if control_response is not None and control_response.get('status') == 'success':  # Убедимся, что control_response не является None
-            if url == '/login':
-                new_location = "/verif_main"  # Перенаправление на main_page при аутентификации
-            else:
-                new_location = "/"  # Перенаправление на home_page при регистрации
-            headers = f"HTTP/1.1 302 Found\nLocation: {new_location}\nContent-Type: text/html; charset=utf-8\n\n"
+        if control_response is not None and control_response.get('status') == 'success':
+            session_id = generate_session_id()  # Генерируем новый session_id
+            new_location = "/verif_main"  # Перенаправляем на /command после успешной аутентификации
+            headers = redirect_to(new_location, session_id)
+            active_sessions[session_id] = {"start_time": time.time()}  # Сохраняем новый session_id в active_sessions
             return headers.encode()
     body = generate_content(code, url)
     response = (headers + body).encode()
-
     return response
 
 
@@ -128,9 +150,10 @@ def run():
         with open('out.txt', 'w') as f:
             f.write(request.decode("UTF-8"))
         response = generate_result(request.decode("UTF-8"))
-
         client_socket.sendall(response)
         client_socket.close()
 
 
 run()
+
+
