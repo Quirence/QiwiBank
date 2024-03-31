439f065d1f618e33ef0110052927002587c9c177
Quirence <shades.h4ndeir@gmail.com>
2024-03-31 17:17:16 +0800

    Сервер готов

--- logs/logs.py
+++ logs/logs.py
@@ -1,22 +1,26 @@
 import os
 import sqlite3
 import json
+from datetime import datetime, timedelta
+
 class Logs:
     def __init__(self):
         db_file = os.path.join(os.path.dirname(__file__), 'requests.db')
         self.conn = sqlite3.connect(db_file)
         self.cursor = self.conn.cursor()
         self.create_table()
+        self.last_cleanup_time = datetime.now()
 
     def create_table(self):
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS logs
                                (id INTEGER PRIMARY KEY AUTOINCREMENT,
                                 request TEXT,
                                 response TEXT,
-                                session TEXT)''')
+                                session TEXT,
+                                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
         self.conn.commit()
 
-    def add_log(self, request, response, session):
+    def add_log(self, request, response, session=None):
         self.cursor.execute("INSERT INTO logs (request, response, session) VALUES (?, ?, ?)", (json.dumps(request), json.dumps(response), json.dumps(session)))
         self.conn.commit()
 
@@ -28,6 +32,13 @@ class Logs:
         else:
             return None, None, None
 
+    def cleanup_database(self):
+        current_time = datetime.now()
+        if current_time - self.last_cleanup_time >= timedelta(weeks=1):
+            self.cursor.execute("DELETE FROM logs WHERE timestamp <= ?", (current_time - timedelta(weeks=1),))
+            self.conn.commit()
+            self.last_cleanup_time = current_time
+
     def close(self):
         self.conn.close()
 
@@ -35,45 +46,12 @@ class Logs:
 # if __name__ == "__main__":
 #     logs = Logs()
 #
-#     requests = [
-#         {'email': 'artiom.burkalov@mail.ru', 'password': 'logobun3', 'method': 'Login'},
-#         {'method': 'Getbalance', 'kind_of_account': 'Debit'},
-#         {'method': 'Openaccount', 'kind_of_account': 'Debit'},
-#         {'method': 'Getbalance', 'kind_of_account': 'Debit'},
-#         {'phoneNumber': '123', 'amount': '1000', 'method': 'Sendmoney', 'kind_of_account': 'Debit'},
-#         {'email': 'artiom.burkalov@mail.ru', 'request': 'GetID'},
-#         {'number': '123', 'request': 'NumberGetID'}
-#     ]
+#     # Предположим, что это пример использования класса и добавление логов происходит регулярно
+#     logs.add_log({'example_request': 'data'}, {'example_response': 'data'})
+#     logs.add_log({'example_request': 'data'}, {'example_response': 'data'})
+#     logs.add_log({'example_request': 'data'}, {'example_response': 'data'})
 #
-#     responses = [
-#         {'status': 'success'},
-#         {'status': 'success', 'balance': 1000},
-#         {'status': 'success', 'id_giver': 2, 'id_receiver': 1, 'amount': 1000}
-#     ]
-#
-#     sessions = [
-#         None,
-#         {'start_time': 1711875769.4994056, 'email': 'test@test.com'},
-#         {'start_time': 1711875769.4994056, 'email': 'test@test.com'},
-#         None,
-#         {'start_time': 1711875769.4994056, 'email': 'test@test.com'},
-#         None,
-#         {'start_time': 1711875769.4994056, 'email': 'test@test.com'}
-#     ]
-#
-#     # Добавляем запросы, ответы и сессии в логи
-#     for req, resp, sess in zip(requests, responses, sessions):
-#         logs.add_log(req, resp, sess)
-#
-#     # Получаем лог по его ID
-#     log_id = 3  # Пример ID лога
-#     request, response, session = logs.get_log_by_id(log_id)
-#     if request and response:
-#         print("Retrieved log:")
-#         print("Request:", request)
-#         print("Response:", response)
-#         print("Session:", session)
-#     else:
-#         print("Log not found for ID:", log_id)
+#     # Выполняем очистку базы данных (если прошла неделя с момента последней очистки)
+#     logs.cleanup_database()
 #
 #     logs.close()
